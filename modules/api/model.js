c74789c9ce18a122b2d0aedf47ebba1ea124720f
Nav989 <94343020+Nav989@users.noreply.github.com>
2021-12-29 18:49:34 +0530

    Update model.js

--- modules/api/model.js
+++ modules/api/model.js
@@ -3,6 +3,9 @@ const { error } = require('winston');
 const db = require('../../database/mysql');
 
 const movie=db.movieData;
+const acheive=db.archeive;
+const book = db.books;
+const populer= db.populer;
 
   const google= (req, res)=> {
     var url = 'http://google.com';
@@ -27,9 +30,21 @@ const movie=db.movieData;
       }
       else {
         res.json(result);
-        console.log(url)
-        console.log("result",result)
+        console.log("result",result.response.docs[1].multimedia[0].subtype)
+        const Data = acheive.create({
+          subtype:result.response.docs[1].multimedia[0].subtype ,
+          type: result.response.docs[1].multimedia[0].type,
+          url: result.response.docs[1].multimedia[0].url,
+          height: result.response.docs[1].multimedia[0].height,
+          width: result.response.docs[1].multimedia[0].width ,
+          crop_name: result.response.docs[1].multimedia[0].crop_name ,
+      });
+      
+      if(!Data)return error;
+      else{
+        console.log("data saved");
       }
+        }
     });
   };
 
@@ -42,7 +57,17 @@ const movie=db.movieData;
       else {
         res.json(result);
         console.log(url)
-        console.log("result",result)
+        const bookData = book.create({
+          list_name:result.results.list_name,
+          bestsellers_date: result.results.bestsellers_date,
+          published_date: result.results.publication_date,
+          rank: result.results.books[0].rank,
+          description: result.results.books[0].description,
+          author_name: result.results.books[0].author,
+      });
+      
+      if(!bookData)return error;
+      console.log("data saved in DB")
       }
     });
   };
@@ -57,6 +82,18 @@ const movie=db.movieData;
         res.json(result);
         console.log(url)
         console.log("result",result)
+        const Data = populer.create({
+          url:result.results[0].url,
+          source: result.results[0].source,
+          published_date: result.results[0].published_date,
+          title: result.results[0].title,
+          abstract: result.results[0].abstract,
+      });
+      
+      if(!Data)return error;
+      else{
+      console.log("data saved in DB");
+      }
       }
     });
   };
